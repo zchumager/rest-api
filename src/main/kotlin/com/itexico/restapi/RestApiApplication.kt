a83dad62324e458ba4482c6e24f537347818a926
Pedro Machado <pedro.machado@tala.co>
2020-01-29 21:37:05 -0600

    spring factory removal fix

--- src/main/kotlin/com/itexico/restapi/RestApiApplication.kt
+++ src/main/kotlin/com/itexico/restapi/RestApiApplication.kt
@@ -1,11 +1,9 @@
 package com.itexico.restapi
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-@EnableAutoConfiguration
 open class RestApiApplication
 
 fun main(args: Array<String>) {
